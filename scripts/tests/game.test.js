a556d98df5bc35ff33f6b31a01d3ed9235794933
Janisroz <janisrozkalejs@gmail.com>
2023-10-10 10:00:04 +0000

    Add final tests

--- scripts/tests/game.test.js
+++ scripts/tests/game.test.js
@@ -2,7 +2,9 @@
  * @jest-environment jsdom
  */
 
-const {game, newGame, showScore, addTurn, lightsOn, showTurns} = require("../game")
+const {game, newGame, showScore, addTurn, lightsOn, showTurns, playerTurn} = require("../game")
+
+jest.spyOn(window, "alert").mockImplementation(() => { });
 
  beforeAll(() => {
      let fs = require("fs");
@@ -27,6 +29,12 @@ const {game, newGame, showScore, addTurn, lightsOn, showTurns} = require("../gam
     });
     test("turnNumber key exists", () => {
         expect("turnNumber" in game).toBe(true);
+    });
+    test("lastButton key exists", () => {
+        expect("lastButton" in game).toBe(true);
+    });
+    test("turnInProgress key exists", () => {
+        expect("turnInProgress" in game).toBe(true);
     });
     test("choices contain correct ids", () => {
         expect(game.choices).toEqual(["button1", "button2", "button3", "button4"])
@@ -87,4 +95,24 @@ const {game, newGame, showScore, addTurn, lightsOn, showTurns} = require("../gam
         showTurns();
         expect(game.turnNumber).toBe(0);
     });
+    test("should increment the score if the turn is correct", () =>{
+        game.playerMoves.push(game.currentGame[0]);
+        playerTurn();
+        expect(game.score).toBe(1);
+    })
+    test("should call an alert if the move is wrong", () => {
+        game.playerMoves.push("wrong");
+        playerTurn();
+        expect(window.alert).toBeCalledWith("Wrong move!")
+    });
+    test("should check if a turn is happening", () => {
+        showTurns();
+        expect(game.turnInProgress).toBe(true);
+    });
+    test("clicking during the computer sequence should fail", () => {
+        showTurns();
+        game.lastButton = "";
+        document.getElementById("button2").click();
+        expect(game.lastButton).toEqual("")
+    })
  });
